3b8958ce09808088a526ae61a0d00a8d0add5976
mdmoren <markmoreno34@gmail.com>
2023-10-21 16:54:03 -0400

    Initial commit

---
+++ SmashPicker/src/main/java/com/SmashPicker/SmashPicker/SmashPickerApplication.java
@@ -0,0 +1,13 @@
+package com.SmashPicker.SmashPicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmashPickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmashPickerApplication.class, args);
+	}
+
+}
---
+++ SmashPicker/src/test/java/com/SmashPicker/SmashPicker/SmashPickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SmashPicker.SmashPicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmashPickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
